20be9be2c694ebf7f75c2f4aa567a9680b507eee
nectordev <p.vlad-rus@mail.ru>
2024-02-08 18:04:31 +0300

    first commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Владислав Пономаренко on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
